791c4a80d139dcbaab9f068e03ca9a7204895218
nicoloddo <gnicoloddo@gmail.com>
2023-12-06 19:41:06 +0100

    Results Exploration Notebooks preparing to be added to the Preprocessing package

--- Exploration notebooks/__libpath.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jun  6 16:50:09 2023
-
-@author: nicol
-"""
-
-import sys
-sys.path.append('../') # This needs to reflect how many folders up the custom libraries are
